960426a7fdc087b0968e7b17e6949ab1d62b1ec3
Sodacooky <sodacooky@foxmail.com>
2022-07-24 15:34:33 +0800

    init

---
+++ src/main/java/sodacooky/txbotj/TxBotJApplication.java
@@ -0,0 +1,13 @@
+package sodacooky.txbotj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TxBotJApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TxBotJApplication.class, args);
+    }
+
+}
